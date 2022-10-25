60e797757e1c95213a56742fd11b270473903346
Facundo Martinez Vidal <facumartinezvidal@gmail.com>
2022-10-25 10:35:26 -0300

    Create 10.25.py

---
+++ Clases/10.25.py
@@ -0,0 +1,17 @@
+# pila = [] #inicializando la pila
+# pila = [3,4,2,1,8] #agregar valores a la pila con .append
+# pila = [-1] #toma el tope
+# pila.pop() #saca el el ultimo valor
+
+#generar una pila con 10 valores aleatorios entre 15 y 50
+#mostrar la pila generada 
+#ir desapilando e ir mostrando sus elementos
+# from random import randint as r
+# pila=[]
+# for x in range(10):
+#     pila.append(r(15,50))
+# print(pila)
+
+# for x in range(len(pila)):
+#     print(pila[-1], end=' ')
+#     pila.pop()
